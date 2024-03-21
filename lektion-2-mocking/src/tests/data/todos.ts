45e3327c7d2612424d3787fcbd9dd207b5b7e8b2
Camilla Landin <landin.camilla8@gmail.com>
2024-03-21 11:10:56 +0100

    Creating todos-variabel to use in tests

---
+++ lektion-2-mocking/src/tests/data/todos.ts
@@ -0,0 +1,9 @@
+import { Todo } from "../../types/Todo";
+
+export const todos: Todo[] = [
+	{ id: 1, title: "My first todo", completed: false },
+	{ id: 2, title: "My second todo", completed: true },
+	{ id: 3, title: "My third todo", completed: false },
+];
+
+export default todos;
