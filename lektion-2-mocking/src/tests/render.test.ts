17a580dc590e3f00dd32fbdfdf956f9a50cd0d61
Camilla Landin <landin.camilla8@gmail.com>
2024-03-21 11:11:14 +0100

    Test for renderfunction

---
+++ lektion-2-mocking/src/tests/render.test.ts
@@ -0,0 +1,21 @@
+import { afterEach, describe, it, expect } from "vitest"
+import { transformTodosToHtml } from "../utils/render";
+
+describe("render todos", () => {
+	it("outputs empty list when no todos exist", () => {
+		const html = transformTodosToHtml([]);
+		expect(html).toBe("");
+	});
+
+	it.todo("outputs a list with one todo", () => {
+
+	});
+
+	it.todo("outputs a list with one completed todo", () => {
+
+	});
+
+	it.todo("outputs a list with two todos", () => {
+
+	});
+});
