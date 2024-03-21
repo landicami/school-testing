15206c2fc2faff7cefbd81b390251040b3c554a6
Camilla Landin <landin.camilla8@gmail.com>
2024-03-21 10:50:11 +0100

    Refaktored render functions

--- lektion-2-mocking/src/utils/render.ts
+++ lektion-2-mocking/src/utils/render.ts
@@ -1,10 +1,21 @@
 import { Todo } from "../types/Todo";
 
+// render output to an HTML element
+export const render = (element: HTMLElement, html: string) => {
+	element.innerHTML = html;
+}
+
 // render todos
 export const renderTodos = (todos: Todo[]) => {
-	// replace todosList content
-	const todosEl = document.querySelector<HTMLUListElement>("#todos")!;
-	todosEl.innerHTML = todos
+	render(
+		document.querySelector<HTMLUListElement>("#todos")!,
+		transformTodosToHtml(todos)
+	);
+}
+
+// transform todos to HTML(-string)
+export const transformTodosToHtml = (todos: Todo[]) => {
+	const html = todos
 		.map(
 			(todo) =>
 				`<li class="list-group-item todo ${todo.completed ? 'completed' : ''}" data-todo-id="${todo.id}">
@@ -16,4 +27,23 @@ export const renderTodos = (todos: Todo[]) => {
 			</li>`
 		)
 		.join("");
+	return html;
 };
+
+// render todos
+// export const renderTodos = (todos: Todo[]) => {
+// 	// replace todosList content
+// 	const todosEl = document.querySelector<HTMLUListElement>("#todos")!;
+// 	todosEl.innerHTML = todos
+// 		.map(
+// 			(todo) =>
+// 				`<li class="list-group-item todo ${todo.completed ? 'completed' : ''}" data-todo-id="${todo.id}">
+// 				<span class="todo-title">${todo.title}</span>
+// 				${todo.completed
+// 					? '<span class="delete-todo" role="button" title="Delete todo">🗑️</span>'
+// 					: ''
+// 				}
+// 			</li>`
+// 		)
+// 		.join("");
+// };
