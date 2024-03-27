884e07fc15f0a356a49bcaa13d599b482b07e100
Camilla Landin <landin.camilla8@gmail.com>
2024-03-27 09:55:34 +0100

    Our handlers installed

---
+++ lektion-4-mocking-api/frontend/src/mocks/handlers.ts
@@ -0,0 +1,18 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const handlers = [
+	// Mock get all todos
+	// GET http://localhost:3001/todos
+
+	// Mock get single todo
+	// GET http://localhost:3001/todos/:todoId
+
+	// Mock create single todo
+	// POST http://localhost:3001/todos
+
+	// Mock update todo
+	// PATCH http://localhost:3001/todos/:todoId
+
+	// Mock delete todo
+	// DELETE http://localhost:3001/todos/:todoId
+];
