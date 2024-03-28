3ab9d6bf21dc5a422c946bda6f7aa9ee84d020ca
Camilla Landin <landin.camilla8@gmail.com>
2024-03-28 10:56:21 +0100

    Adding cypress-tests for next.js application

--- lektion-5-cypress/cypress/e2e/home.cy.ts
+++ lektion-5-cypress/cypress/e2e/home.cy.ts
@@ -1,5 +1,24 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Home Page", () => {
+	beforeEach(() => {
+		cy.visit("http://localhost:3000");
+
+		// wait for the app to be fully hydrated and visible
+		cy.get("#__next").should("be.visible");
+	});
+
+	it("the h1 contains the correct text", () => {
+		// cy.get('[data-test="hero-heading"]')  // 🫤
+		cy.get("h1")
+			.should("exist")
+			.contains("Testing Next.js Applications with Cypress");
+	});
+
+	it("the features on the homepage are correct", () => {
+		// Find the first definition term
+		cy.get("dt").eq(0).contains("4 Courses");
+
+		cy.get("dt").eq(1).contains("25+ Lessons");
+
+		cy.get("dt").eq(2).contains("Free and Open Source");
+	});
+});
