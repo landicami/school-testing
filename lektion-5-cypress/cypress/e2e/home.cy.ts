05c096427506a623ac3657ae930f5acf83135963
Camilla Landin <landin.camilla8@gmail.com>
2024-03-28 13:38:50 +0100

    Adding context-instruction for heropage

--- lektion-5-cypress/cypress/e2e/home.cy.ts
+++ lektion-5-cypress/cypress/e2e/home.cy.ts
@@ -6,19 +6,21 @@ describe("Home Page", () => {
 		cy.get("#__next").should("be.visible");
 	});
 
-	it("the h1 contains the correct text", () => {
-		// cy.get('[data-test="hero-heading"]')  // 🫤
-		cy.get("h1")
-			.should("exist")
-			.contains("Testing Next.js Applications with Cypress");
-	});
+	context("Hero section", () => {
+		it("the h1 contains the correct text", () => {
+			// cy.get('[data-test="hero-heading"]')  // 🫤
+			cy.get("h1")
+				.should("exist")
+				.contains("Testing Next.js Applications with Cypress");
+		});
 
-	it("the features on the homepage are correct", () => {
-		// Find the first definition term
-		cy.get("dt").eq(0).contains("4 Courses");
+		it("the features on the homepage are correct", () => {
+			// Find the first definition term
+			cy.get("dt").eq(0).contains("4 Courses");
 
-		cy.get("dt").eq(1).contains("25+ Lessons");
+			cy.get("dt").eq(1).contains("25+ Lessons");
 
-		cy.get("dt").eq(2).contains("Free and Open Source");
-	});
+			cy.get("dt").eq(2).contains("Free and Open Source");
+		});
+	})
 });
