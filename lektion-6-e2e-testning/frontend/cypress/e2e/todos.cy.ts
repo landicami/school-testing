f5d4817e29a8c1805e9f0b9a48e6046998b15ddc
Camilla Landin <landin.camilla8@gmail.com>
2024-04-02 10:56:04 +0200

    Writing tests for todos

--- lektion-6-e2e-testning/frontend/cypress/e2e/todos.cy.ts
+++ lektion-6-e2e-testning/frontend/cypress/e2e/todos.cy.ts
@@ -1,5 +1,37 @@
 describe("trying out todos", () => {
-  it('can get the localhost page', () => {
-    cy.visit("/")
-  })
+	beforeEach(() => {
+		cy.visit("/")
+
+	})
+
+	it.skip('should see at least one todo', () => {
+		cy.get('#todos').find("li").should("have.length.at.least", 1)
+	})
+
+	it.only("create todo form should be empty", () => {
+		cy.get('#new-todo-title').should("have.value", "");
+	});
+
+	it.skip("can't create a todo without a title", () => {
+		cy.get('#new-todo-title').type(" ")
+		cy.get('.btn-success').click()
+		cy.get('#error')
+		.should("be.visible")
+		.contains("Title must be at least 3 characters long")
+	});
+
+	it.skip("can create a new todo (and see it in the list and clears input)", () => {
+		const newTodo = "The new todo"
+		cy.get('#new-todo-title').type(newTodo)
+		cy.get('.btn-success').click()
+		cy.get('#todos').find("li")
+		.last()
+		.contains(newTodo)
+		cy.get('#new-todo-title').should("be.empty");
+	});
+
+	it.skip("can type in the create todo form and then reset the form", () => {
+		cy.get('#new-todo-title').type("Create a new todo")
+		cy.get('.btn-warning').click();
+	});
 })
