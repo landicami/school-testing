e641da21bf6eade9041d8e860af31b1eb7cca459
Camilla Landin <landin.camilla8@gmail.com>
2024-04-02 09:47:53 +0200

    Changing what to ignore

--- lektion-6-e2e-testning/frontend/cypress.config.ts
+++ lektion-6-e2e-testning/frontend/cypress.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
 	excludeSpecPattern: [
-		"**/e2e/1-getting-started",
-		"**/e2e/2-advanced-examples"
+		"**/e2e/examples",
 	],
 	baseUrl: "http://localhost:5173/",
     setupNodeEvents(on, config) {
