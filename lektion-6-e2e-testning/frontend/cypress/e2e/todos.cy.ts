8c24bbf578a1bbc5ebc073d9d43a91422cf43a59
Camilla Landin <landin.camilla8@gmail.com>
2024-04-02 11:32:59 +0200

    Updated tests

--- lektion-6-e2e-testning/frontend/cypress/e2e/todos.cy.ts
+++ lektion-6-e2e-testning/frontend/cypress/e2e/todos.cy.ts
@@ -8,27 +8,45 @@ describe("trying out todos", () => {
 		cy.get('#todos').find("li").should("have.length.at.least", 1)
 	})
 
-	it.only("create todo form should be empty", () => {
+	it.skip("create todo form should be empty", () => {
 		cy.get('#new-todo-title').should("have.value", "");
 	});
 
 	it.skip("can't create a todo without a title", () => {
-		cy.get('#new-todo-title').type(" ")
-		cy.get('.btn-success').click()
+		// cy.get('#new-todo-title').type(" ")
+
+		cy.get('.btn-success').contains("Create").click()
 		cy.get('#error')
 		.should("be.visible")
-		.contains("Title must be at least 3 characters long")
+		.contains("Title cannot be empty")
 	});
 
-	it.skip("can create a new todo (and see it in the list and clears input)", () => {
+	it.only("can create a new todo (and see it in the list and clears input)", () => {
 		const newTodo = "The new todo"
 		cy.get('#new-todo-title').type(newTodo)
 		cy.get('.btn-success').click()
+		cy.wait(1300)
 		cy.get('#todos').find("li")
 		.last()
 		.contains(newTodo)
-		cy.get('#new-todo-title').should("be.empty");
+		cy.get('#new-todo-title').should("have.value", "");
 	});
+
+	// it.skip( även köra cy.wait
+	// 	"can create a new todo (and see it in the list and clears input)",
+	// 	{ defaultCommandTimeout: 6000 }, () => {
+	// 		const todoTitle = "Too many todos, did not read";
+
+	// 		// type a todo title and submit form
+	// 		cy.get("#new-todo-title").type(todoTitle);
+	// 		cy.get("[type=\"submit\"]").click();
+
+	// 		// expect that a todo with the title exists in the list
+	// 		cy.get("#todos").find("li").last().contains(todoTitle);
+
+	// 		// expect input to be empty
+	// 		cy.get("#new-todo-title").should("have.value", "");
+	// });
 
 	it.skip("can type in the create todo form and then reset the form", () => {
 		cy.get('#new-todo-title').type("Create a new todo")
