5c03b98ab39b82e87c44a6f18a278d5ad1eb36f7
Camilla Landin <landin.camilla8@gmail.com>
2024-04-04 11:38:25 +0200

    adding custom commands

--- lektion-9-e2e-102/cypress/support/commands.ts
+++ lektion-9-e2e-102/cypress/support/commands.ts
@@ -1,37 +1,34 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+
+declare namespace Cypress {
+    interface Chainable<Subject = any> {
+        login(email: string, password: string): Chainable<Subject>;
+    }
+
+}
+
+
+Cypress.Commands.add('login', (email, password) => {
+	cy.visit("/login");
+	cy.get("input[type=\"email\"]").type(email)
+	cy.get("input[type=\"password\"]").type(password)
+	cy.get("[type=\"submit\"]").click()
+	cy.wait(1000)
+})
+
+/**
+		 * Custom command to log out a user.
+		 *
+		 * @example cy.logout()
+		 */
+declare namespace Cypress {
+	interface Chainable<Subject = any> {
+		logout(): Chainable<JQuery<HTMLElement>>
+	}
+}
+
+Cypress.Commands.add("logout", () => {
+	cy.visit("/logout");
+	cy.wait(1500);
+});
