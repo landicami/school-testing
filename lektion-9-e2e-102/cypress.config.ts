f726fabf429a842d1aa9bb1195f755dba680dfb9
Camilla Landin <landin.camilla8@gmail.com>
2024-04-04 11:37:52 +0200

    adding baseurl

--- lektion-9-e2e-102/cypress.config.ts
+++ lektion-9-e2e-102/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+	baseUrl: "https://fed22-firebase-todos.netlify.app/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
