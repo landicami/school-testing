bebd30b5bd329ea8f314dfad44a05090fd213ddf
Camilla Landin <landin.camilla8@gmail.com>
2024-04-04 11:37:45 +0200

    Adding custom commands for login and logout

--- lektion-5-cypress/cypress/support/commands.ts
+++ lektion-5-cypress/cypress/support/commands.ts
@@ -4,8 +4,23 @@ declare namespace Cypress {
 	interface Chainable {
 	  getByData(dataTestAttribute: string): Chainable<JQuery<HTMLElement>>
 	}
+
   }
+
+declare namespace Cypress {
+    interface Chainable<Subject = any> {
+        login(email: string, password: string): Chainable<Subject>;
+    }
+}
+
 
   Cypress.Commands.add("getByData", (selector) => {
 	return cy.get(`[data-test=${selector}]`)
   })
+
+  Cypress.Commands.add('login', (email, password) => {
+		cy.get("input[type=\"email\"]").type(email)
+		cy.get("input[type=\"password\"]").type(password)
+		cy.get("[type=\"submit\"]").click()
+	})
+
